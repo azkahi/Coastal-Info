59a3d6462fd0dcb41ff19df12a5db5e72afb910a
azkahi <azkaimtiyaz@gmail.com>
2017-07-17 19:56:15 +0700

    Added picker page and data passing to the result page.

---
+++ Coastal Info/Coastal_Info/Models/CoastalData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coastal_Info.Models
+{
+    class CoastalData
+    {
+        public string Region { get; set; }
+        public string Station { get; set; }
+        public DateTime DateStart { get; set; }
+        public DateTime DateEnd { get; set; }
+        public bool Prediction { get; set; }
+    }
+}
--- Coastal Info/Coastal_Info/Views/MainPage.xaml.cs
+++ Coastal Info/Coastal_Info/Views/MainPage.xaml.cs
@@ -4,14 +4,98 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Coastal_Info.Models;
 
 namespace Coastal_Info.Views
 {
     public partial class MainPage : ContentPage
     {
+        private List<String> Regions = new List<string>()
+        {
+            "A (Western)",
+            "B (Central)",
+            "C (Eastern)"
+        };
+
+        private List<String> Stations = new List<string>()
+        {
+            "Badas",
+            "Balikpapan",
+            "Banten (Ciwandan)",
+            "Binuangeun",
+            "Carik",
+            "Celukan Bawang",
+            "Cirebon"
+        };
+
+        private List<String> Predictions = new List<string>()
+        {
+            "Yes",
+            "No"
+        };
+
         public MainPage()
         {
             InitializeComponent();
+            PopulatePickers();
+        }
+
+        private void PopulatePickers()
+        {
+            foreach (string s in Regions)
+            {
+                RegionPicker.Items.Add(s);
+            }
+
+            foreach (string s in Stations)
+            {
+                StationPicker.Items.Add(s);
+            }
+
+            foreach (string s in Predictions)
+            {
+                PredictionPicker.Items.Add(s);
+            }
+        }
+
+        public async void SearchButton(object sender, EventArgs e)
+        {
+            if (RegionPicker.SelectedIndex == -1)
+            {
+                await DisplayAlert("Region Not Selected", "Please select a region.", "Ok");
+                return;
+            }
+            if (StationPicker.SelectedIndex == -1)
+            {
+                await DisplayAlert("Station Not Selected", "Please select a station.", "Ok");
+                return;
+            }
+            if (StationPicker.SelectedIndex == -1)
+            {
+                await DisplayAlert("Prediction Not Selected", "Please select a prediction.", "Ok");
+                return;
+            }
+
+            CoastalData coastalData = new CoastalData
+            {
+                Region = RegionPicker.Items[RegionPicker.SelectedIndex],
+                Station = StationPicker.Items[StationPicker.SelectedIndex],
+                DateStart = DateStartPicker.Date,
+                DateEnd = DateEndPicker.Date,
+            };
+
+            string _prediction = PredictionPicker.Items[PredictionPicker.SelectedIndex];
+            if (_prediction == "Yes")
+            {
+                coastalData.Prediction = true;
+            } else
+            {
+                coastalData.Prediction = false;
+            }
+
+            ResultPage resultPage = new ResultPage();
+            resultPage.BindingContext = coastalData;
+            await Navigation.PushModalAsync(resultPage);
         }
     }
 }
